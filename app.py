6e60f01f37d24926c9a57adc3e312473a1a9dd01
TheCodingBrie <gbelangerbrie@gmail.com>
2021-08-01 16:15:37 +0200

    testing the ssh key

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from flask import Flask, redirect, render_template, request
 from helpers import error
 
 app = Flask(__name__)
-
+ 
 @app.route("/")
 def index():
-    return error("Too much Kool-Aid")
-    # return render_template("index.html.j2")
-    # return render_template("test.html")
-
+    return render_template("index.html.j2")
